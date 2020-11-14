b0926e4faca14bf22d83f8380e31a5535aae1db3
KateL212 <ka.kovjarova@gmail.com>
2020-11-15 00:34:30 +0300

    update ADL middleware with new user data

--- app/middlewares/acl.py
+++ app/middlewares/acl.py
@@ -11,12 +11,20 @@ class ACLMiddleware(BaseMiddleware):
     @staticmethod
     async def setup_chat(data: dict, user: types.User, chat: Optional[types.Chat] = None):
         user_id = user.id
+        first_name = user.first_name
+        last_name = user.last_name
+        username = user.username
+        language = user.language_code
         chat_id = chat.id if chat else user.id
         chat_type = chat.type if chat else "private"
 
-        user = await User.get(user_id)
+        user: User = await User.get(user_id)
         if user is None:
-            user = await User.create(id=user_id)
+            user = await User.create(id=user_id,
+                                     first_name=first_name,
+                                     last_name=last_name,
+                                     username=username,
+                                     language=language)
         chat = await Chat.get(chat_id)
         if chat is None:
             chat = await Chat.create(id=chat_id, type=chat_type)
