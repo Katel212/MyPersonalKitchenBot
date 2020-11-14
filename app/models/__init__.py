843f4422a647e98eccfa0dc8f9e25c7dfd4bfdfc
KateL212 <ka.kovjarova@gmail.com>
2020-11-15 00:02:56 +0300

    add shopping list model and migration

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 from .user import User
 from .chat import Chat
 from .product import Product
+from .shopping_list import ShoppingList
---
+++ app/models/shopping_list.py
@@ -0,0 +1,8 @@
+from app.misc import db
+from app.models.user import UserRelatedModel
+
+
+class ShoppingList(UserRelatedModel):
+    __tablename__ = "shopping_lists"
+
+    id = db.Column(db.Integer, primary_key=True, index=True, unique=True)
