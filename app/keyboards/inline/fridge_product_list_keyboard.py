a5f7e83d1d121dafc5dbecabad639a63e0b07e9a
KateL212 <ka.kovjarova@gmail.com>
2020-11-29 15:20:46 +0300

    fix buttons in fridge_product_list_keyboard.py and rename files

--- app/keyboards/inline/fridge_product_list_keyboard.py
+++ app/keyboards/inline/fridge_product_list_keyboard.py
@@ -8,18 +8,18 @@ from app.models import Product
 class ProductListKeyboard(InlineKeyboardMarkup):
 
     @staticmethod
-    def create(products: 'typing.List[Product]', page: int, previous_button:str) -> InlineKeyboardMarkup:
-        product_buttons = [InlineKeyboardButton(product.name, callback_data=f"show_{previous_button}_{product.id}") for product in
+    def create(products: 'typing.List[Product]', page: int, previous_button: str) -> InlineKeyboardMarkup:
+        product_buttons = [[InlineKeyboardButton(product.name, callback_data=f"show_{previous_button}_{product.id}")] for product in
                            products[page * 10:(page + 1) * 10]]
-
         previous_page_button = InlineKeyboardButton('<<', callback_data=f'previous_page_{previous_button}')
-        next_page_button = InlineKeyboardButton('>>', callback_data=f'next_page_fridge{previous_button}')
-        add_button = InlineKeyboardButton('Добавить', callback_data=f'add_to_fridge{previous_button}')
+        next_page_button = InlineKeyboardButton('>>', callback_data=f'next_page_{previous_button}')
+        add_button = InlineKeyboardButton('Добавить', callback_data=f'add_to_{previous_button}')
         last_row_buttons = []
         if page != 0:
             last_row_buttons.append(previous_page_button)
         last_row_buttons.append(add_button)
         if (page + 1) * 10 < len(products):
             last_row_buttons.append(next_page_button)
-        keyboard = InlineKeyboardMarkup(inline_keyboard=[product_buttons, last_row_buttons])
+
+        keyboard = InlineKeyboardMarkup(inline_keyboard=[*product_buttons, last_row_buttons])
         return keyboard
--- app/keyboards/inline/product_for_shop_list_keyboard.py
+++ app/keyboards/inline/product_for_shop_list_keyboard.py
@@ -3,9 +3,9 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from app.models import Product
 
 
-class ProductInfoKeyboard(InlineKeyboardMarkup):
+class ShoppingListProductInfoKeyboard(InlineKeyboardMarkup):
     @staticmethod
     def create(product: Product):
         delete_from_list_button = InlineKeyboardButton('Удалить из списка', callback_data=f'delete_from_shopping_list_{product.id}')
-        buy_button = InlineKeyboardButton('Купить', callback_data=f'add_to_fridge_{product.id}')
+        buy_button = InlineKeyboardButton('Купить', callback_data=f'buy_{product.id}')
         return InlineKeyboardMarkup(inline_keyboard=[delete_from_list_button, buy_button])
