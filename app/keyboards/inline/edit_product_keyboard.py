d46da58b03f67396ee0a7f4f6fc165660c10eab4
KateL212 <ka.kovjarova@gmail.com>
2021-01-30 15:14:00 +0300

    fix edit_product_keyboard reply markup

--- app/keyboards/inline/edit_product_keyboard.py
+++ app/keyboards/inline/edit_product_keyboard.py
@@ -11,4 +11,4 @@ class ChangeInfoAboutProductKeyboard(InlineKeyboardMarkup):
         change_info_button = InlineKeyboardButton('Изменить информацию о продукте', callback_data=f'change_info_{product.id}')
         change_bought_time_button = InlineKeyboardButton('Изменить дату покупку', callback_data=f'change_bought_time_{product.id}')
         return InlineKeyboardMarkup(
-            inline_keyboard=[change_name_button, change_expiration_date_button, change_bought_time_button, change_info_button])
+            inline_keyboard=[[change_name_button], [change_expiration_date_button], [change_bought_time_button], [change_info_button]])
