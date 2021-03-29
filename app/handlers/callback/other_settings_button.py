0561262db264fc658859f2bfe65154b151c4e4fc
CyberSniff <sonyglebova4@gmail.com>
2021-03-29 22:24:56 +0300

    create default list

--- app/handlers/callback/other_settings_button.py
+++ app/handlers/callback/other_settings_button.py
@@ -1,6 +1,8 @@
 import re
+from datetime import datetime, timedelta
 
 from aiogram import types
+
 
 from aiogram.dispatcher import filters
 
@@ -9,6 +11,7 @@ from app.misc import dp, bot
 from app.keyboards.inline.automatic_setting_of_the_purchase_date_keyboard import \
     AutomaticSettingOfThePurchaseDateListKeyboard
 from app.keyboards.inline.default_list_of_products_keyboard import DefaultListOfProductsListKeyboard
+from app.models import Product
 
 
 @dp.callback_query_handler(filters.Regexp(r'other_(default_list_of_products|auto_set_purchase_date)'))
@@ -40,8 +43,104 @@ async def default_list_callback_handler(query: types.CallbackQuery):
     groups = re.match(r'default_list_(on|off)', query.data).groups()
     source = groups[0]
     if source == 'on':
+        new_product = await Product.create(user_id=query.from_user.id,
+                                           name='Хлеб',
+                                           expiration_date=datetime.today() + timedelta(days=1),
+                                           bought_date=datetime.today(),
+                                           info='')
+        new_product = await Product.create(user_id=query.from_user.id,
+                                           name='Мука пшеничная',
+                                           expiration_date=datetime.today() + timedelta(days=183),
+                                           bought_date=datetime.today(),
+                                           info='')
+        new_product = await Product.create(user_id=query.from_user.id,
+                                           name='Сода',
+                                           expiration_date=datetime.today() + timedelta(days=365),
+                                           bought_date=datetime.today(),
+                                           info='')
+        new_product = await Product.create(user_id=query.from_user.id,
+                                           name='Соль',
+                                           expiration_date=datetime.today() + timedelta(days=730),
+                                           bought_date=datetime.today(),
+                                           info='')
+        new_product = await Product.create(user_id=query.from_user.id,
+                                           name='Сахар',
+                                           expiration_date=datetime.today() + timedelta(days=730),
+                                           bought_date=datetime.today(),
+                                           info='')
+        new_product = await Product.create(user_id=query.from_user.id,
+                                           name='Черный перец',
+                                           expiration_date=datetime.today() + timedelta(days=155),
+                                           bought_date=datetime.today(),
+                                           info='')
+        new_product = await Product.create(user_id=query.from_user.id,
+                                           name='Макароны',
+                                           expiration_date=datetime.today() + timedelta(days=365),
+                                           bought_date=datetime.today(),
+                                           info='')
+        new_product = await Product.create(user_id=query.from_user.id,
+                                           name='Гречневая каша',
+                                           expiration_date=datetime.today() + timedelta(days=365),
+                                           bought_date=datetime.today(),
+                                           info='')
+        new_product = await Product.create(user_id=query.from_user.id,
+                                           name='Рис',
+                                           expiration_date=datetime.today() + timedelta(days=365),
+                                           bought_date=datetime.today(),
+                                           info='')
+        new_product = await Product.create(user_id=query.from_user.id,
+                                           name='Масло подсолнечное',
+                                           expiration_date=datetime.today() + timedelta(days=45),
+                                           bought_date=datetime.today(),
+                                           info='')
+        new_product = await Product.create(user_id=query.from_user.id,
+                                           name='Масло сливочное',
+                                           expiration_date=datetime.today() + timedelta(days=35),
+                                           bought_date=datetime.today(),
+                                           info='')
+        new_product = await Product.create(user_id=query.from_user.id,
+                                           name='Лук репчатый',
+                                           expiration_date=datetime.today() + timedelta(days=12),
+                                           bought_date=datetime.today(),
+                                           info='')
+        new_product = await Product.create(user_id=query.from_user.id,
+                                           name='Чеснок',
+                                           expiration_date=datetime.today() + timedelta(days=121),
+                                           bought_date=datetime.today(),
+                                           info='')
+        new_product = await Product.create(user_id=query.from_user.id,
+                                           name='Дрожжи',
+                                           expiration_date=datetime.today() + timedelta(days=31),
+                                           bought_date=datetime.today(),
+                                           info='')
+        new_product = await Product.create(user_id=query.from_user.id,
+                                           name='Молоко',
+                                           expiration_date=datetime.today() + timedelta(days=2),
+                                           bought_date=datetime.today(),
+                                           info='')
+        new_product = await Product.create(user_id=query.from_user.id,
+                                           name='Яйца(куриные)',
+                                           expiration_date=datetime.today() + timedelta(days=7),
+                                           bought_date=datetime.today(),
+                                           info='')
+        new_product = await Product.create(user_id=query.from_user.id,
+                                           name='Чай',
+                                           expiration_date=datetime.today() + timedelta(days=365),
+                                           bought_date=datetime.today(),
+                                           info='')
+        new_product = await Product.create(user_id=query.from_user.id,
+                                           name='Кофе',
+                                           expiration_date=datetime.today() + timedelta(days=365),
+                                           bought_date=datetime.today(),
+                                           info='')
+        new_product = await Product.create(user_id=query.from_user.id,
+                                           name='Яйца(куриные)',
+                                           expiration_date=datetime.today() + timedelta(days=365),
+                                           bought_date=datetime.today(),
+                                           info='')
         await bot.send_message(query.from_user.id, 'Загружено',
                                )
+
     elif source == 'off':
         await bot.send_message(query.from_user.id, 'Удален',
                                )
